40b7761e469c8fb8ee51692e188f59665cc238a6
Serge <justsegaekb@yandex.ru>
2024-06-28 15:13:34 +0500

    add filtering categories

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,15 +1,18 @@
 import styles from './styles.module.css'
 import NewsBanner from "../../components/NewsBanner/NewsBanner.jsx";
 import {useEffect, useState} from "react";
-import {getNews} from "../../api/apiNews.js";
+import {getCategories, getNews} from "../../api/apiNews.js";
 import NewsList from "../../components/NewsList/NewsList.jsx";
 import Skeleton from "../../components/Skeleton/Skeleton.jsx";
 import Pagination from "../../components/Pagination/Pagination.jsx";
+import Categories from "../../components/Categories/Categories.jsx";
 
 const Main = () => {
     const
         [news, setNews] = useState([]),
         [isLoading, setIsLoading] = useState(true),
+        [categories, setCategories] = useState([]),
+        [selectedCategory, setSelectedCategory] = useState('All'),
         [currentPage, setCurrentPage] = useState(1),
         totalPages = 10,
         pageSize = 10;
@@ -17,9 +20,22 @@ const Main = () => {
     const fetchNews = async (currentPage) => {
         try {
             setIsLoading(true)
-            const response = await getNews(currentPage, pageSize);
+            const response = await getNews({
+                page_number: currentPage,
+                page_size: pageSize,
+                category: selectedCategory === 'All' ? null : selectedCategory,
+            });
             setNews(response.news);
             setIsLoading(false)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const fetchCategories = async () => {
+        try {
+            const response = await getCategories();
+            setCategories( ['All', ...response.categories]);
         } catch (e) {
             console.log(e)
         }
@@ -43,9 +59,15 @@ const Main = () => {
 
     useEffect(() => {
         fetchNews(currentPage);
-    }, [currentPage])
+    }, [currentPage, selectedCategory])
+
+    useEffect(() => {
+        fetchCategories()
+    }, []);
+
     return (
         <main className={styles.main}>
+            <Categories categories={categories} selectedCategory={selectedCategory} setSelectedCaregory={setSelectedCategory} />
             { news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Skeleton type={'banner'} count={1} />}
             <Pagination handleNextPage={handleNextPage}
                         totalPages={totalPages}
